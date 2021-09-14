13218ce8c7cb441008e2c770f1a2a0f35357c60c
Alena DENIS <basstet067@yahoo.com>
2021-09-14 16:10:11 +0200

    feat(popup.js, index.js, index.html, style.css): popups

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable strict */
+'use strict';
+
+import togglePopup from './modules/popup';
+
+// модальные окна:
+togglePopup();
---
+++ js/modules/popup.js
@@ -0,0 +1,32 @@
+const togglePopup = () => {
+  const openPopup = event => {
+    if (!event.target.matches('.fancyboxModal')) {
+      return;
+    }
+    event.preventDefault();
+    const modalBtn = event.target,
+          popupContent = document.querySelector(modalBtn.getAttribute('href')),
+          popup = popupContent.closest('.modal--opened'),
+          overlay = document.querySelector('.overlay');
+
+    popup.style.display = 'block';
+    overlay.style.display = 'block';
+    popupContent.style.display = 'block';
+
+    const closePopup = event => {
+      if (!event.target.matches('.modal__close, .overlay')) {
+        return;
+      }
+      popup.style.display = 'none';
+      overlay.style.display = 'none';
+      popupContent.style.display = 'none';
+      document.body.removeEventListener('click', closePopup);
+    };
+
+    document.body.addEventListener('click', closePopup);
+  };
+  
+  document.body.addEventListener('click', openPopup);
+};
+
+export default togglePopup;
