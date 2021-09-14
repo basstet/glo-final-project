319bfcba885968d394bcd33f01e7265ceac335a8
Alena DENIS <basstet067@yahoo.com>
2021-09-14 16:58:10 +0200

    feat(scrollTop.js, index.js, style.css): scroll to top

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 'use strict';
 
 import togglePopup from './modules/popup';
+import scrollTop from './modules/scrollTop';
 
 // модальные окна:
 togglePopup();
+// кнопка плавной прокрутки вверх:
+scrollTop();
